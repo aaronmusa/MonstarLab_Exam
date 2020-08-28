6fd2b4f033e406470e478c32461374b19ee63019
Aaron Musa <amusa@aso.ph>
2020-08-28 15:38:08 +0800

    Initial Commit

---
+++ MonstarLab_Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonstarLab_Exam
+//
+//  Created by Aaron Musa on 8/28/20.
+//  Copyright © 2020 Aaron Musa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
